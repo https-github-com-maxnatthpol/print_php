774c6202dde009aa4f280ea60c52890985d4e072
mikeaom <56056527+mikeaom@users.noreply.github.com>
2021-03-21 20:27:03 +0700

    d

--- functions.php
+++ functions.php
@@ -247,7 +247,7 @@ function printslip_return_customer_his()
       $text = "---------------------------------------------------";
       $text = iconv("UTF-8","TIS-620",$text);
       
-      printer_draw_text($handle, $text, dpimm2px(11), dpimm2px(19));
+      printer_draw_text($handle, $text, dpimm2px(0.1), dpimm2px(19));
         $text = "เลขบัตร";
       $text = iconv("UTF-8","TIS-620",$text);
       
